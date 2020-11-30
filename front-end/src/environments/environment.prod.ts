b70885609fd5a49c44f1f11f0ef38325b2979f60
CYOSP <cyosp@cyosp.com>
2020-11-30 07:40:56 +0100

    Remove ids front-end files

--- front-end/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  backEndLocation: 'http://localhost:8080',
-  directoryReversedOrder: true,
-  previewDirectoryReversedOrder: false,
-  mixDirectoriesAndImages: false
-};
